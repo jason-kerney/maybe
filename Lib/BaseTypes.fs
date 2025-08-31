92c6dea8b980db4d3977189f244e0c19185ecae8
Jason Kerney <Jason@TeamKerney.com>
2025-08-31 14:07:49 -0700

    doc comments

--- Lib/BaseTypes.fs
+++ Lib/BaseTypes.fs
@@ -1,23 +1,57 @@
 ﻿namespace Utils.Maybe
 
+/// <summary>
+/// Represents the possible failure cases for a process.
+/// </summary>
 type ProcessFailure =
+    /// <summary>Failure due to an exception.</summary>
     | ExceptionFailure of exn
+    /// <summary>Failure with a general error message.</summary>
     | GeneralFailure of string
+    /// <summary>Failure due to a combination of multiple failures.</summary>
     | CombinationError of ProcessFailure list
 
+/// <summary>
+/// Alias for a result type with <typeparamref name='Value'/> or <see cref='ProcessFailure'/>.
+/// </summary>
 type maybe<'Value> = Result<'Value, ProcessFailure>
+
+/// <summary>
+/// Synonym for <c>maybe&lt;'Value&gt;</c> for convenience.
+/// </summary>
 type Maybe<'Value> = maybe<'Value>
+
+/// <summary>
+/// A maybe-wrapped list of values.
+/// </summary>
 type mlist<'Value> = maybe<'Value list>
 
+/// <summary>
+/// A maybe-wrapped sequence of values.
+/// </summary>
 type mseq<'Value> = maybe<'Value seq>
 
 [<AutoOpen>]
 module BaseHelpers =
+    /// <summary>
+    /// Wraps a string as a <see cref='GeneralFailure'/> in a <c>maybe</c> error.
+    /// </summary>
     let asGeneralFailure failure: _ maybe = failure |> GeneralFailure |> Error
+
+    /// <summary>
+    /// Wraps an exception as an <see cref='ExceptionFailure'/> in a <c>maybe</c> error.
+    /// </summary>
     let asExceptionFailure failure: _ maybe = failure |> ExceptionFailure |> Error
+
+    /// <summary>
+    /// Wraps a list of failures as a <see cref='CombinationError'/> in a <c>maybe</c> error.
+    /// </summary>
     let asCombinationFailure failures: _ maybe =
         failures |> CombinationError |> Error
 
+    /// <summary>
+    /// Combines two <see cref='ProcessFailure'/> values into a single <see cref='CombinationError'/>.
+    /// </summary>
     let combineWith failure1 failure2 =
         match failure1, failure2 with
         | CombinationError f1, CombinationError f2 ->
@@ -30,8 +64,14 @@ module BaseHelpers =
         | f1, f2 ->
             [f1; f2] |> asCombinationFailure
 
+    /// <summary>
+    /// Synonym for <c>combineWith</c>.
+    /// </summary>
     let asFailureCombinedWith = combineWith
             
+    /// <summary>
+    /// Applies a maybe-wrapped function to a maybe-wrapped value.
+    /// </summary>
     let apply (maybeF: _ maybe) (maybeX: _ maybe) : _ maybe = 
         match maybeF,maybeX with
         | Ok f, Ok x -> Ok (f x)
--- Lib/Maybe.fs
+++ Lib/Maybe.fs
@@ -1,54 +1,104 @@
-﻿module Utils.Maybe.Maybe
+﻿
+/// <summary>
+/// Core Maybe utilities for working with the 'maybe' type.
+/// </summary>
+module Utils.Maybe.Maybe
 
+/// <summary>
+/// Wraps a value in a <c>maybe</c> (Ok) type.
+/// </summary>
 let wrap : _ -> _ maybe = Ok
     
+/// <summary>
+/// Returns the first successful (Ok) value from two <c>maybe</c> values, or the second if both are errors.
+/// </summary>
 let orMaybe (a: _ maybe) (b: _ maybe) : _ maybe =
     match b, a with
     | Ok v, _
     | _, Ok v -> Ok v
     | _ -> b
     
+/// <summary>
+/// Returns <paramref name="a"/> if it is Ok, otherwise <paramref name="b"/>.
+/// </summary>
 let isPreferredOver (a: _ maybe) (b: _ maybe) : _ maybe =
     b |> orMaybe a
     
+/// <summary>
+/// Returns <paramref name="b"/> if it is Ok, otherwise <paramref name="a"/>.
+/// </summary>
 let butPrefer (a: _ maybe) (b: _ maybe) : _ maybe =
     a |> orMaybe b
 
+/// <summary>
+/// Flattens a nested <c>maybe</c> value (i.e., <c>maybe maybe</c>) into a single <c>maybe</c>.
+/// </summary>
 let simplify (value: _ maybe maybe) =
     maybe {
         let! value = value
         return! value
     }
 
+/// <summary>
+/// Converts a <c>bool maybe</c> to a <c>bool</c>, returning false if Error.
+/// </summary>
 let toBool (value: bool maybe) =
     match value with
     | Ok v -> v
     | Error _ -> false
     
+/// <summary>
+/// Returns true if the <c>maybe</c> value is Ok.
+/// </summary>
 let hasValue (possible: _ maybe) =
     match possible with
     | Ok _ -> true
     | _ -> false
     
+/// <summary>
+/// Returns true if the <c>maybe</c> value is Error.
+/// </summary>
 let isError (value: _ maybe) =
     match value with
     | Ok _ -> false
     | _ -> true
     
+/// <summary>
+/// Returns the value if Ok, otherwise returns the provided default value.
+/// </summary>
 let orDefault defaultValue (value: _ maybe) =
     match value with
     | Ok v -> v
     | _ -> defaultValue
     
+/// <summary>
+/// Lifts a function to operate on a <c>maybe</c> value.
+/// </summary>
 let lift f : _ maybe -> _ maybe = Result.map f
+
+/// <summary>
+/// Lifts a function returning <c>maybe</c> to operate on a <c>maybe</c> value (monadic bind).
+/// </summary>
 let mlift (f: _ -> _ maybe) : _ maybe -> _ maybe = Result.bind f
 
+/// <summary>
+/// Applies a lifted function to a <c>maybe</c> value.
+/// </summary>
 let llift value f = (lift f) value
 
+/// <summary>
+/// Applies a monadic lifted function to a <c>maybe</c> value.
+/// </summary>
 let mllift value f = (mlift f) value
 
+/// <summary>
+/// Partially lifts a function returning <c>maybe</c> to operate on a <c>maybe</c> value.
+/// </summary>
 let partialLift (f: _ -> _ maybe) : _ maybe -> _ maybe = Result.bind f
 
+/// <summary>
+/// Maps a function over a <c>maybe</c> value using computation expressions.
+/// </summary>
 let map f (item: _ maybe) =
     maybe {
         let! item = item
@@ -57,15 +107,24 @@ let map f (item: _ maybe) =
             f item
     }
     
+/// <summary>
+/// Checks a predicate on a <c>maybe</c> value, returning a bool.
+/// </summary>
 let check f item =
     item
     |> map f
     |> toBool
     
+/// <summary>
+/// Converts an option to a <c>maybe</c>, using the provided error message if None.
+/// </summary>
 let fromOption errorMsg : _ option -> _ maybe = function
     | Some v -> Ok v
     | _ -> errorMsg |> asGeneralFailure
     
+/// <summary>
+/// Converts a <c>maybe</c> value to a string representation.
+/// </summary>
 let callToString : _ maybe -> string = function
     | Ok v -> v.ToString ()
     | Error e -> e |> Error |> sprintf "%A"
