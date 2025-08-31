a8e003e5a2e83e2f8d2a87b55a2cbaca9741f2d0
Jason Kerney <Jason@TeamKerney.com>
2025-08-31 14:13:54 -0700

    doc comments

--- Lib/MaybeSeq.fs
+++ Lib/MaybeSeq.fs
@@ -1,6 +1,16 @@
-﻿module Utils.Maybe.MaybeSeq
+﻿
+/// <summary>
+/// Maybe-aware sequence operations and helpers.
+/// </summary>
+module Utils.Maybe.MaybeSeq
 
+/// <summary>
+/// Reduces a sequence of maybe values, combining errors if present. Returns Ok () if no errors.
+/// </summary>
 let reduceErrors (items: _ maybe seq) =
+    /// <summary>
+    /// Helper to wrap a list of failures as a combination failure or Ok ().
+    /// </summary>
     let maybeFailures (items: _ list) =
         if 0 < items.Length then items |> asCombinationFailure
         else Ok ()
@@ -8,5 +18,5 @@ let reduceErrors (items: _ maybe seq) =
     items
     |> Seq.toList
     |> List.filter Maybe.isError
-    |> List.map (fun (Error e) -> e)
+    |> List.map (function Error e -> e | _ -> failwith "Unexpected Ok in error filter")
     |> maybeFailures
